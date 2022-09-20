22f77b90b3384db947e89d94333f95de756df002
vkalyaanasundaram <vkalyaanasundaram@kapitus.com>
2022-09-20 12:43:55 +0530

    home page changes are reverted back

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 import Header from "../components/Header";
 import Hero from "../components/Hero";
@@ -8,8 +8,9 @@ import Footer from "../components/Footer";
 import { client } from '../lib/apollo';
 import { gql } from "@apollo/client";
 
+
 export default function Home({ page }) {
-  const [transparent, setTransparent] = useState(false)
+  const [transparent, setTransparent] = useState(false);
 
   return (
     // <div className={styles.container}>
@@ -22,19 +23,19 @@ export default function Home({ page }) {
       <Header />
       <main className="mx-5 my-10">
         <>
-        <Hero
-          title="Get Started Some Loans"
-          buttonText="GET STARTED"
-          buttonURL="https://faustjs.org"
-          button2Text={null}
-          button2URL={null}
-          bgImage="https://res.cloudinary.com/dsfu88hae/image/upload/v1647389853/NewMedia/linkedin-sales-solutions-VtKoSy_XzNU-unsplash_vrlzaa.webp"
-          id={styles.home_hero} 
-     
-          slug="home"
-        >
-        </Hero>
-          <div> {page.title}</div>
+          <Hero
+            title="Get Started Some Loans"
+            buttonText="GET STARTED"
+            buttonURL="https://faustjs.org"
+            button2Text={null}
+            button2URL={null}
+            bgImage="https://res.cloudinary.com/dsfu88hae/image/upload/v1647389853/NewMedia/linkedin-sales-solutions-VtKoSy_XzNU-unsplash_vrlzaa.webp"
+            id={styles.home_hero}
+
+            slug="home"
+          >
+          </Hero>
+          {/* <div> {page.title}</div> */}
           <div dangerouslySetInnerHTML={{ __html: page.content ?? "" }} />
         </>
       </main>
