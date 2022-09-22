14d02bde2541387d03824b5ac6e672cfe841b4d5
vkalyaanasundaram <vkalyaanasundaram@kapitus.com>
2022-09-22 19:52:53 +0530

    partner pageURI added

--- pages/partner/[pageUri].js
+++ pages/partner/[pageUri].js
@@ -1,12 +1,55 @@
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import Head from 'next/head';
+import Image from 'next/image';
+import { client } from '../../lib/apollo';
+import { gql } from "@apollo/client";
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+import AdvancedHero from "../../components/AdvancedHero"
 
-export default function PartnerPage() {
+export default function PartnerPage({ data }) {
     return (
         <>
             <Header />
-            <div className="my-10 mx-5">Partner Page</div>
+            <AdvancedHero
+                title={data?.title}
+                indexTitle=''
+                // bgImage={productsService?.heroAdvanced?.desktopBanner?.sourceUrl()}
+                bgImage={data?.featuredImage?.node?.sourceUrl}
+                column='two'
+                slug={data?.slug}
+            />
+            <div dangerouslySetInnerHTML={{ __html: data?.content }}></div>
             <Footer />
         </>
     )
+}
+
+export async function getStaticProps(context) {
+    const { params: { pageUri } } = context;
+    const pageURI = context.params.pageUri;
+    const GET_PAGE = gql`{
+        page(id: "/partner/${pageURI}", idType: URI) {
+          link
+          slug
+          title
+          content
+        }
+      }`
+    const response = await client.query({
+        query: GET_PAGE
+    });
+    return {
+        props: {
+            data: response?.data?.page,
+        }
+    }
+}
+
+
+export async function getStaticPaths() {
+    return {
+        paths: [],
+        fallback: true,
+        // fallback: false
+    };
 }
