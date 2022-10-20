06243ebfe5b3282287453c60952912bd03dd293f
vkalyaanasundaram <vkalyaanasundaram@kapitus.com>
2022-10-20 19:08:23 +0530

    content build with graphql query

--- pages/products-we-offer.js
+++ pages/products-we-offer.js
@@ -12,24 +12,19 @@ import { useInView } from "react-cool-inview";
 import AdvancedHero from '../components/AdvancedHero';
 import { useRouter } from 'next/router';
 import Header from "../components/Header";
+import LastCTA from '../components/LastCTA';
 import Footer from "../components/Footer";
+import CommonCard from "../components/CommonCard";
+import Carousel from "../components/Carousel";
+import Intro from "../components/Intro";
 
 export default function Products({ page, username, password, products, modelTest }) {
     const router = useRouter();
 
-    // console.log(ProductsContent?.page({id: "products-we-offer" })?.title());
-    // const da = ProductsContent?.pages({ first: 100 })?.nodes;
-    // const productsBanner = useQuery().modelTests().nodes;
-    // let productBannerTitle = "";
-    // da.map((val, key) => {
-    //     if (currentSlugName == val.slug) {
-    //         productsBanner.map((v) => {
-    //             if (v.slug == "products-we-offer") {
-    //                 productBannerTitle = v?.mainTitle;
-    //             }
-    //         })
-    //     }
-    // })
+    const cardGroup = [page?.CardRows?.cardGroup1, page?.CardRows?.cardGroup2, page?.CardRows?.cardGroup3]
+    const cardGroup1 = [page?.CardRows?.cardGroup4, page?.CardRows?.cardGroup5, page?.CardRows?.cardGroup6]
+    const cardGroup2 = [page?.CardRows?.cardGroup7, page?.CardRows?.cardGroup8, page?.CardRows?.cardGroup9]
+    const carousel = [page?.carousel?.carouselGroup1, page?.carousel?.carouselGroup2, page?.carousel?.carouselGroup3]
 
 
 
@@ -44,36 +39,54 @@ export default function Products({ page, username, password, products, modelTest
 
     return (
         <>
-            {/* <SEO
-        title={(generalSettings)}
-        // imageUrl={mainBanner?.sourceUrl}
-      /> */}
-            <Header />
 
-            {/* <SecondaryMenu products={products} /> */}
-            <SecondaryMobileMenu products={products} />
+            <Header
+                // title={generalSettings.title}
+                // description={generalSettings.description}
+                // metaDesc={page?.seo?.metaDesc}
+                // opengraphTitle={page?.seo?.metaTitle}
+                // targetKeywords={page?.seo?.targetKeywords}
+                output={{}}
+            />
+            <SecondaryMenu title="" />
+            {/* FIRST SECTION OF PAGE */}
+            <AdvancedHero
+                title={page?.heroAdvanced?.advancedBannerEditor}
+                indexTitle=''
+                bgImage={page?.heroAdvanced?.desktopBanner?.url}
+                slug={page?.slug}
+            />
+            <section ref={observe}>
+                {inView && <Intro
+                    introTitle={page?.intro?.introTitle}
+                    subtitle={page?.intro?.introDescription}
+                />}
+            </section>
 
+            <section ref={observe}>
+                {inView && <CommonCard
+                    cardMainTitle={page?.CardRows?.cardMainTitle}
+                    cardData={cardGroup}
+                    cardData1={cardGroup1}
+                    cardData2={cardGroup2}
+                />}
+            </section>
+            <section ref={observe}>
+                {inView && <Carousel data={carousel} />}
+            </section>
 
-
-            {/* {ProductsContent.map((value, key) => { */}
-
-            <div className={styles.entry}>
-                <section className={`${styles.bgImage}`} >
-
-                    <AdvancedHero
-                        title={modelTest?.mainTitle}
-                        indexTitle=''
-                        bgImage={page?.featuredImage?.node?.sourceUrl}
-                        column='two'
-                        slug={page?.slug}
-                    />
-                </section>
-            </div>
-            <div dangerouslySetInnerHTML={{ __html: page?.content }}></div>
-            <section ref={observe}>{inView && <Footer
-                username={username} password={password}
-            />}</section>
-
+            <section ref={observe}>
+                {inView && <LastCTA
+                    title={page?.finalCta?.finalSectionTitle}
+                    subtitle={''}
+                    description={page?.finalCta.finalSectionDescription}
+                    buttonText={page?.finalCta?.finalSectionButton?.title}
+                    buttonURL={page?.finalCta?.finalSectionButton?.url}
+                />}
+            </section>
+            {/* Mobile Products */}
+            {/* <Products /> */}
+            <section ref={observe}>{inView && <Footer  />}</section>
         </>
     )
 }
@@ -82,16 +95,208 @@ export async function getStaticProps() {
     const GET_PAGE = gql`
     {
         page(id: "/products-we-offer", idType: URI) {
-          title
-          slug
-          content
-          uri
-          featuredImage {
-            node {
-              sourceUrl
+            title
+            slug
+            heroAdvanced {
+                mobileBanner {
+                altText
+                sourceUrl
+                }
+                desktopBanner {
+                target
+                title
+                url
+                }
+                advancedBannerEditor
             }
-          }
-        }
+            heroBasic {
+                desktopBanner {
+                title
+                url
+                }
+                mobileBanner {
+                sourceUrl
+                altText
+                }
+            }
+            CardRows {
+                cardGroup1 {
+                cardTitle
+                cardDescription
+                cardButton {
+                    url
+                    title
+                    target
+                }
+                cardIcon {
+                    altText
+                    sourceUrl
+                    title
+                }
+                }
+                cardGroup2 {
+                cardTitle
+                cardDescription
+                cardButton {
+                    url
+                    title
+                    target
+                }
+                cardIcon {
+                    altText
+                    sourceUrl
+                    title
+                }
+                }
+                cardMainTitle
+                cardGroup3 {
+                cardTitle
+                cardDescription
+                cardButton {
+                    url
+                    title
+                    target
+                }
+                cardIcon {
+                    altText
+                    sourceUrl
+                    title
+                }
+                }
+                cardGroup4 {
+                cardTitle
+                cardDescription
+                cardButton {
+                    url
+                    title
+                    target
+                }
+                cardIcon {
+                    altText
+                    sourceUrl
+                    title
+                }
+                }
+                cardGroup5 {
+                cardTitle
+                cardDescription
+                cardButton {
+                    url
+                    title
+                    target
+                }
+                cardIcon {
+                    altText
+                    sourceUrl
+                    title
+                }
+                }
+                cardGroup6 {
+                cardTitle
+                cardDescription
+                cardButton {
+                    url
+                    title
+                    target
+                }
+                cardIcon {
+                    altText
+                    sourceUrl
+                    title
+                }
+                }
+                cardGroup7 {
+                cardTitle
+                cardDescription
+                cardButton {
+                    url
+                    title
+                    target
+                }
+                cardIcon {
+                    altText
+                    sourceUrl
+                    title
+                }
+                }
+                cardGroup8 {
+                cardTitle
+                cardDescription
+                cardButton {
+                    url
+                    title
+                    target
+                }
+                cardIcon {
+                    altText
+                    sourceUrl
+                    title
+                }
+                }
+                cardGroup9 {
+                cardTitle
+                cardDescription
+                cardButton {
+                    url
+                    title
+                    target
+                }
+                cardIcon {
+                    altText
+                    sourceUrl
+                    title
+                }
+                }
+            }
+            carousel {
+                carouselGroup1 {
+                companyTitle
+                testimonial
+                carouselImageMobile {
+                    altText
+                    sourceUrl
+                }
+                carouselImageDesktop {
+                    altText
+                    sourceUrl
+                }
+                }
+                carouselGroup2 {
+                companyTitle
+                testimonial
+                companyOwnerName
+                carouselImageMobile {
+                    altText
+                    sourceUrl
+                }
+                carouselImageDesktop {
+                    altText
+                    sourceUrl
+                }
+                }
+                carouselGroup3 {
+                carouselImageDesktop {
+                    altText
+                    sourceUrl
+                }
+                carouselImageMobile {
+                    altText
+                    sourceUrl
+                }
+                companyTitle
+                testimonial
+                }
+            }
+            finalCta {
+                finalSectionTitle
+                finalSectionDescription
+                finalSectionButton {
+                url
+                title
+                target
+                }
+            }
+            }
         modelTest(id: "/products-we-offer", idType: URI) {
            mainTitle
         }
